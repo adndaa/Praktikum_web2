4337d3048b01d9bd277c4f9c2d8a886235c387c8
adndaa <adindasyhptri@gmail.com>
2024-06-05 14:21:32 +0800

    upload ikon

--- profil.php
+++ profil.php
@@ -2,32 +2,32 @@
 <div class="row">
     <div class="col-md-12">
         <div class="text-center">
-            <img src="img/Faishal.jpg" alt="Muhammad Faishal" width="160" class="rounded-circle">
+            <img src="img/adinda.jpg" alt="adinda" width="160" class="rounded-circle">
             <table class="table mt-4" style="width:100%; margin:0 auto;">
                 <tbody>
                     <tr>
-                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
-                        <td align="left" width="50%">Muhammad Faishal</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50%"><strong>NPM :</strong></td>
-                        <td align="left" width="50%">2110010094</td>
-                    </tr>
-                    <tr>
-                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
-                        <td align="left" width="50%">Sikui, 21 Juli 2003</td>
+                        <td align="right" width="50%"><strong>NPM:</strong></td>
+                        <td align="left" width="50%">2210010353</td>
                     </tr>
                     <tr>
                         <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
                         <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
                     </tr>
                     <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Adinda Syah Putri</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Tarakan, 20 Desember 2003</td>
+                    </tr>
+                    <tr>
                         <td align="right" width="50%"><strong>Telepon :</strong></td>
-                        <td align="left" width="50%">082252173503</td>
+                        <td align="left" width="50%">082252169810</td>
                     </tr>
                     <tr>
                         <td align="right" width="50%"><strong>Email :</strong></td>
-                        <td align="left" width="50%">muhammadfaishal325@gmail.com</td>
+                        <td align="left" width="50%">adindasyhptri@gmail.com</td>
                     </tr>
                     <tr>
                         <td align="right" width="50%"><strong>Media Sosial :</strong></td>
